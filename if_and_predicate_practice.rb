b2e7ef64b4c11ca3f99b08d2762977dc0e7eaba9
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-23 22:34:36 -0400

    Add files via upload

---
+++ if_and_predicate_practice.rb
@@ -0,0 +1,15 @@
+if 5.odd?
+    puts "the number is odd"
+  end 
+  
+  #bc the number is odd ruby will output "the number is odd" 
+  
+  word = "kangaroo"
+  
+  if word.include?("roo")
+    puts "That substring does exist"
+  end
+  
+  #because "roo" is in kangaroo, the if statement with output "That substring does exist" 
+  #if the syntax asked for "zebra", nothing will be outputted since zebra is not in the word "kangaroo" 
+  

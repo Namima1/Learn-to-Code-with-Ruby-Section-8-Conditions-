b01e24b26ad5a7102078ea0b80f848013994ddb3
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-23 21:39:01 -0400

    Add files via upload

---
+++ Section_8_Condiitons_If_Statement_practice.rb
@@ -0,0 +1,13 @@
+if 5 < 7 
+  puts "That math statement is true"
+end 
+
+if 5 < 3
+    puts "yes"
+end 
+
+
+#if an if statement is not true ruby will not run
+#in the above statements ruby ran "That math statement is true" because
+#5 is less than 7 just like what the statement above said
+#but in the second statement ruby did not output "yes" because the statement above that was incorrect 

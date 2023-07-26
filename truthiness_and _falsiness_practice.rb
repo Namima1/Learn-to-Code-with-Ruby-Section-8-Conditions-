74661f722e79cec45e283888a6a67db9d2250b76
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-26 14:00:46 -0400

    Add files via upload

---
+++ truthiness_and _falsiness_practice.rb
@@ -0,0 +1,14 @@
+#In ruby there is only 2 falsy values - false, nil 
+#Everything else is truthy 
+
+if nil
+    puts "Will this print?"
+end 
+
+#Nil respresents nothing. Nil is a false value. If the above string is executed nothing will be outputted. 
+
+if 5 
+puts "Will this print?"
+end
+
+#Any integer is true. 

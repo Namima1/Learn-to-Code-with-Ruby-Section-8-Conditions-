7664846df84b1ba9c7a2b9e15752fe30d1fec230
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-27 11:13:51 -0400

    Add files via upload

---
+++ else_if_practice.rb
@@ -0,0 +1,16 @@
+#else and elsif can only be used after if
+
+grade = "A"
+
+if grade == "C"
+  puts "Mom will be happy" 
+else 
+  puts "Mom will be upset"  
+end 
+
+#else lets us execute some bit of logic 
+#else is garanteed to execute if, the if statement is false.
+#elsif is not garanteed to execute  
+
+#In the code above ruby the if grade statement is false so it will move onto 
+#the else statement 

2ec9b5f37ceb15b5c420cbe662ab7d175855ec4b
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-31 12:04:05 -0400

    Add files via upload

---
+++ multiple_conditions_with_or_practice.rb
@@ -0,0 +1,12 @@
+#OR
+# ||
+#or validates that either one or the other condition is true
+
+entree = "Steak"
+price = 49.99
+
+if entree == "Steak" ||  price < 29.99
+  puts "At least one of the conditions is true, purchasing meal" 
+end
+
+#the puts statement did output because one of the statements is true.  

b28b30c1659ddc3cb5299074683eac3cf97aba9d
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-08-07 22:01:14 -0400

    Add files via upload

---
+++ ternary_operator_practice.rb
@@ -0,0 +1,16 @@
+#ternary - composed of three parts
+#it consists of three segments 
+#shortcut for if and els statements
+#allows us to simplify lines into just one 
+
+if 1 < 2
+  puts "Yes, it is"
+else
+  puts "No, it's not"
+end
+
+value = puts 1 < 2 ? "Yes, it is" : "No, it's not" 
+puts value 
+
+#? is part of the ternary operator. It is asking is 1 < 2 
+#: can only be use if it is a if else 
